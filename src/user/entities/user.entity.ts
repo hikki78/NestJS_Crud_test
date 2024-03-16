08b2329a986c3814d6c75922a527830551dc9d14
root <root@Morpheus>
2024-03-17 03:43:21 +0530

    nest is fun fr fr

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+  } from 'typeorm';
+  
+  @Entity()
+  export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    fullName: string;
+  
+    @Column({ unique: true })
+    email: string;
+  }
